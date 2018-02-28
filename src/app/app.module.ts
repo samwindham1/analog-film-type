673e57cd29c825c67b305f0e1fa89a03f3f48024
Sam Windham <sam.windham@arrow.com>
2018-02-27 17:02:20 -0700

    basic component setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FilmComponent } from './film/film.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FilmComponent
   ],
   imports: [
     BrowserModule

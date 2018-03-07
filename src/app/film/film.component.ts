1f21c17a28bb9ccdf8578e1b2486e7c7e9cf6842
samwindham1 <sam.windham95@gmail.com>
2018-03-07 16:19:27 -0700

    Film component styling

--- src/app/film/film.component.ts
+++ src/app/film/film.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-
 import { ApiService } from '../../api.service';
 
 @Component({
@@ -9,8 +8,12 @@ import { ApiService } from '../../api.service';
 })
 
 export class FilmComponent implements OnInit {
+  private data;
   private posts;
   private film_types;
+
+  private init_count = 10;
+  private loaded_count = 10;
 
   constructor(private api: ApiService) { }
 
@@ -18,20 +21,28 @@ export class FilmComponent implements OnInit {
     console.log(film);
     if (film === 'default') {
       this.api.get_all().subscribe(res => {
-        this.posts = res.slice(0, 10);
+        this.data = res;
+        this.posts = this.data.slice(0, this.init_count);
         console.log(this.posts);
       });
     } else {
       this.api.get_film(film).subscribe(res => {
-        this.posts = res.slice(0, 10);
+        this.data = res;
+        this.posts = this.data.slice(0, this.init_count);
         console.log(this.posts);
       })
     }
   }
 
+  onLoadMore() {
+    this.posts.concat(this.data.slice(this.loaded_count, this.loaded_count + this.init_count));
+    this.loaded_count += this.init_count;
+  }
+
   ngOnInit() {
     this.api.get_all().subscribe(res => {
-      this.posts = res.slice(0, 10);
+      this.data = res;
+      this.posts = this.data.slice(0, this.init_count);
       console.log(this.posts);
     });
     this.api.get_film_types().subscribe(f => {
